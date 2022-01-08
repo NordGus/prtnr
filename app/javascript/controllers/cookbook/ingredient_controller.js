7ed7eea7246352c4862691a05fa1a3076057ef7b
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2022-01-08 22:27:37 +0100

    Refactoring Recipe Ingrediente Flow:   - polishing ingredient_controller.js.

--- app/javascript/controllers/cookbook/ingredient_controller.js
+++ app/javascript/controllers/cookbook/ingredient_controller.js
@@ -4,25 +4,26 @@ export default class IngredientController extends Controller {
     static targets = [ 'id', 'preview', 'file', 'name', 'result', 'fileName' ]
 
     select(event) {
-        const target = event.currentTarget
-        const { id, preview, filename, name } = target.dataset
+        const { id, preview, filename, name } = event.currentTarget.dataset
 
         this.fileTarget.files = null
+        this._togglePreview(true)
 
-        this.idTarget.value = Number(id) || ''
-        this.nameTarget.value = name || ''
-        // this.fileTarget.files = file || ''
-        this.previewTarget.src = preview || ''
-        this.fileNameTarget.innerText = filename || ''
+        this.idTarget.value           = Number(id) || ''
+        this.nameTarget.value         = name       || ''
+        this.previewTarget.src        = preview    || ''
+        this.fileNameTarget.innerText = filename   || ''
+
+        this._togglePreview(this.previewTarget.src === '')
 
         this._focusSelected(Number(id))
     }
 
     clear(_event) {
-        this.idTarget.value = ''
-        this.nameTarget.value = ''
-        this.fileTarget.files = null
-        this.previewTarget.src = ''
+        this.idTarget.value           = ''
+        this.nameTarget.value         = ''
+        this.fileTarget.files         = null
+        this.previewTarget.src        = ''
         this.fileNameTarget.innerText = ''
     }
 
@@ -32,5 +33,9 @@ export default class IngredientController extends Controller {
 
             resultTarget.classList.toggle('is-focused', id === resultId)
         }
+    }
+
+    _togglePreview(hide) {
+        this.previewTarget.classList.toggle('is-hidden', hide)
     }
 }
